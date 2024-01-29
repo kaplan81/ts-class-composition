2633d42d90094501fe9cdea1faa2bd287dcfa9d0
“kaplan81” <gesteira.pro@gmail.com>
2024-01-29 13:05:23 +0100

    create state mixin

--- projects/mixins/component-with-mixins.ts
+++ projects/mixins/component-with-mixins.ts
@@ -1,3 +1,10 @@
+import {
+  BehaviorSubject,
+  MonoTypeOperatorFunction,
+  Observable,
+  shareReplay,
+} from 'rxjs';
+
 /**
  * This is needed to run the code
  * in a Node.js execution context.
@@ -14,24 +21,52 @@ if ((globalThis as any).performance.nodeTiming.name === 'node') {
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type Constructor<T = unknown> = new (...args: any[]) => T;
 
-interface Composed {
+interface Composed extends State<EntityExample> {
   menuOpen: boolean;
-  state: State;
   toggleMenu(): void;
 }
 
-interface State {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  entity: any | null;
+interface State<E> extends NgrxEntityState<E> {
   loaded: boolean;
   loading: boolean;
 }
 
-const initialState: State = {
-  entity: null,
+interface NgrxEntityState<E> {
+  ids: string[] | number[];
+  entities: { [id: string | number]: E };
+}
+
+interface EntityExample {
+  id: string | number;
+  entity: {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    data: any;
+  };
+}
+
+interface StateMixinFacade<S> {
+  state$: Observable<S>;
+  resetState(): void;
+  updateState(state: S): void;
+}
+
+const initialState: State<EntityExample> = {
+  ids: [1],
+  entities: {
+    [1]: {
+      id: 1,
+      entity: {
+        data: {},
+      },
+    },
+  },
   loaded: false,
   loading: false,
 };
+
+function shareReplayForMulticast<T>(): MonoTypeOperatorFunction<T> {
+  return shareReplay({ bufferSize: 1, refCount: true });
+}
 
 // function CustomElementDecorator<T extends Constructor<HTMLElement>>(
 //   tagName: string
@@ -58,6 +93,37 @@ const initialState: State = {
 //       : target;
 //   };
 // }
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export function StateMixin<T extends Constructor<{}>>(
+  Base: T,
+  initialState: State<EntityExample>
+): Constructor<StateMixinFacade<State<EntityExample>>> & T {
+  return class extends Base {
+    #dispatch$: BehaviorSubject<State<EntityExample>>;
+    #initialState: State<EntityExample> = initialState;
+    state$: Observable<State<EntityExample>>;
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    constructor(...args: any[]) {
+      super(...args);
+      this.#dispatch$ = new BehaviorSubject<State<EntityExample>>(
+        this.#initialState
+      );
+      this.state$ = this.#dispatch$
+        .asObservable()
+        .pipe(shareReplayForMulticast());
+    }
+
+    resetState(): void {
+      this.#dispatch$.next(this.#initialState);
+    }
+
+    updateState(state: State<EntityExample>): void {
+      this.#dispatch$.next(state);
+    }
+  };
+}
 
 // // eslint-disable-next-line @typescript-eslint/ban-types
 // function MenuDecorator<T extends Constructor<{}>>(): (
