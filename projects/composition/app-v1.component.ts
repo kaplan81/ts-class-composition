bcc60b8b73d4982aa01bf18bff773eef71c40808
“kaplan81” <gesteira.pro@gmail.com>
2024-02-05 12:19:31 +0100

    refine first version of app

--- projects/composition/app-v1.component.ts
+++ projects/composition/app-v1.component.ts
@@ -1,43 +1,41 @@
 (() => {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   type Constructor<T = {}> = new (...args: any[]) => T;
 
   type Rendering = 'eager' | 'lazy';
 
   interface Composed {
-    r: Rendering;
+    rendering: Rendering;
   }
 
-  // eslint-disable-next-line @typescript-eslint/ban-types
   function Decorator<B extends Constructor<{}>>(
     rendering: Rendering
-  ): (base: B, ctx: ClassDecoratorContext) => B {
+  ): (base: B, context: ClassDecoratorContext) => B {
     return (Base: B, context: ClassDecoratorContext) => {
       return context.kind === 'class'
         ? class extends Base {
-            r: Rendering = rendering;
+            rendering: Rendering = rendering;
           }
         : Base;
     };
   }
 
-  interface C extends Composed {}
+  interface AppComponent extends Composed {}
   @Decorator('lazy')
-  class C {
-    p = 'property';
+  class AppComponent {
+    property = 'property';
 
     constructor() {
-      console.log('constructor');
+      console.log('Component');
     }
 
-    m(): string {
+    method(): string {
       return 'method';
     }
   }
 
-  const c = new C();
+  const component = new AppComponent();
 
-  console.log('C p:::', c.p);
-  console.log('C m:::', c.m());
-  console.log('C r:::', c.r);
+  console.log('C p:::', component.property);
+  console.log('C m:::', component.method());
+  console.log('C r:::', component.rendering);
 })();
